08ae8d338300302e2c30fe83dab035f42ea8c986
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-09-25 09:48:40 +0530

    Responsive contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,38 +2,41 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="border border-stone-200 shadow-md w-2/4 rounded-md">
-      <h1 className="font-bold font-mono justify-center flex text-xl">
+    <div className="border border-stone-200 shadow-md w-full sm:w-3/4 md:w-2/4 rounded-md mx-auto absolute bottom-7 left-0 right-0">
+      <h1 className="font-bold font-mono justify-center flex text-xl py-4">
         Contact Us
-      </h1>
-      <form action="" className="flex flex-col p-2 m-2">
-        <div className="flex justify-center space-x-11">
+      </h1> 
+      <form
+        action=""
+        className="flex flex-col p-4"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <div className="flex flex-col sm:flex-row justify-center sm:space-x-1 space-y-4 sm:space-y-0">
           <input
             type="text"
             name=""
             id=""
             placeholder="Name"
-            className="bg-gray-200 placeholder-gray-600 placeholder:px-2 rounded-lg placeholder:py-2 p-2 w-56"
+            className="bg-gray-200 placeholder-gray-600 rounded-lg p-2 w-full sm:w-2/6"
           />
           <input
             type="email"
             name=""
             id=""
             placeholder="Email"
-            className="bg-gray-200 placeholder-gray-600 placeholder:px-2 palceholder:py-2 rounded-lg p-2 w-56"
+            className="bg-gray-200 placeholder-gray-600 rounded-lg p-2 w-full  sm:w-2/6"
           />
         </div>
-        <div className="flex justify-center pt-4 ">
+        <div className="flex justify-center pt-4">
           <textarea
-            className="bg-gray-200 placeholder:p-3  placeholder-gray-600 rounded-lg w-4/6 h-32 "
+            className="bg-gray-200 placeholder-gray-600 rounded-lg w-full sm:w-4/6 h-32 p-3"
             name=""
             id=""
             placeholder="Message"
           ></textarea>
         </div>
-
-        <div className="flex justify-center pt-2 ">
-          <button className="bg-teal-700 p-2 rounded-md my-2  flex justify-center w-4/6  ">
+        <div className="flex justify-center pt-4">
+          <button className="bg-teal-700 text-white p-2 rounded-md my-2 w-full sm:w-4/6">
             Send
           </button>
         </div>
